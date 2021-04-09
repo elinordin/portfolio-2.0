89963423266723a58387e8bdf78462e2cb882cb1
elinordin <elin.evelina@live.se>
2021-04-09 13:49:33 +0200

    Add initial styles

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,19 +8,17 @@ import cv from '../assets/icons/cv.png'
 function About() {
     return (
         <section className='about'>
-            <div>
-                <h1>Hi there, I'm Elin</h1>
-                <p>I am a <span>creative problem solver</span> and <span>frontend developer</span>.
+            <div className='text'>
+                <h1>Hi there, I'm <span>Elin Nordin</span></h1>
+                <p>I am a <span>creative problem solver</span> and <span>frontend developer</span>. <br/>
                 Make yourself at home and if you feel the urge to <span>explore</span> more of
                 my <span>brain</span>, <span>heart</span> or <span>soul</span>, reach out on the links below!</p>
-            </div>
-            <div>
+
                 <a href='mailto:elin.nordin@hyperisland.se' aria-label='Email me'><img src={mail}/></a>
                 <a href='https://github.com/elinordin' aria-label='Go to my github'><img src={github}/></a>
                 <a href='https://www.linkedin.com/in/elinordin/' aria-label='Go to my linkedin'><img src={linkedin}/></a>
                 <a href='' aria-label='Download my CV'><img src={cv}/></a>
             </div>
-
         </section>
     );
 }
