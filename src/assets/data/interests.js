84e8e3d6a4ba578bf1407868d0b6c8b31322d061
elinordin <elin.evelina@live.se>
2021-04-13 17:19:00 +0200

    Refactor interests data

--- src/assets/data/interests.js
+++ src/assets/data/interests.js
@@ -1,42 +1,47 @@
-const speed = [1, 2, 3, 4]
+const speed = ['2s', '3s', '4s', '5s']
 const fills = ['#41A3EE', '#85E6D0', '#FEB943', '#FDD961']
+
+const getRandomFill = () => {return fills[Math.floor(Math.random() * fills.length)]}
+const getRandomPosition = () => {return Math.floor(Math.random() * 80) + '%'}
+const getRandomSpeed = () => {return speed[Math.floor(Math.random() * speed.length)]}
+
 
 
 const interests = [
     {
         key: '1',
         interest: 'Dancing',
-        fill: fills[Math.floor(Math.random() * fills.length)],
-        position: Math.floor(Math.random() * 100) + '%',
-        speed: speed[Math.floor(Math.random() * speed.length)],
+        fill: getRandomFill(),
+        position: getRandomPosition(),
+        speed: getRandomSpeed(),
     },
     {
         key: '2',
         interest: 'Bullet journal',
-        fill: fills[Math.floor(Math.random() * fills.length)],
-        position: Math.floor(Math.random() * 100) + '%',
-        speed: speed[Math.floor(Math.random() * speed.length)],
+        fill: getRandomFill(),
+        position: getRandomPosition(),
+        speed: getRandomSpeed(),
     },
     {
         key: '3',
         interest: 'Hiking',
-        fill: fills[Math.floor(Math.random() * fills.length)],
-        position: Math.floor(Math.random() * 100) + '%',
-        speed: speed[Math.floor(Math.random() * speed.length)],
+        fill: getRandomFill(),
+        position: getRandomPosition(),
+        speed: getRandomSpeed(),
     },
     {
         key: '4',
         interest: 'Salsa',
-        fill: fills[Math.floor(Math.random() * fills.length)],
-        position: Math.floor(Math.random() * 100) + '%',
-        speed: speed[Math.floor(Math.random() * speed.length)],
+        fill: getRandomFill(),
+        position: getRandomPosition(),
+        speed: getRandomSpeed(),
     },
     {
         key: '5',
         interest: 'Food',
-        fill: fills[Math.floor(Math.random() * fills.length)],
-        position: Math.floor(Math.random() * 100) + '%',
-        speed: speed[Math.floor(Math.random() * speed.length)],
+        fill: getRandomFill(),
+        position: getRandomPosition(),
+        speed: getRandomSpeed(),
     },
 ]
 
