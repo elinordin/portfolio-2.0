d5dbc42962ba1ed2133346c3f1de1217b71b77ef
elinordin <elin.evelina@live.se>
2021-04-14 15:31:25 +0200

    Add more interests

--- src/assets/data/interests.js
+++ src/assets/data/interests.js
@@ -1,4 +1,4 @@
-const sizes = ['0.7rem', '0.8rem', '0.9rem', '1rem', '1.1rem']
+const sizes = ['0.7rem', '0.8rem', '0.9rem', '1rem']
 const fills = ['#41A3EE', '#85E6D0', '#FEB943', '#FDD961']
 
 const getRandomFill = () => {return fills[Math.floor(Math.random() * fills.length)]}
@@ -6,7 +6,7 @@ const getRandomSize = () => {return sizes[Math.floor(Math.random() * sizes.lengt
 const getRandomPosition = () => {return Math.floor(Math.random() * 80) + '%'}
 const getRandomSvgSpeed = () => {return Math.floor((Math.random() * 6) + 1) + 's'}
 const getRandomMoveSpeed = () => {return Math.floor((Math.random() * 25) + 20) + 's'}
-const getDelay = () => {return Math.floor(Math.random() * 4) + 's'}
+const getDelay = () => {return Math.floor(Math.random() * 8) + 's'}
 
 
 
@@ -54,6 +54,106 @@ const interests = [
     {
         key: '5',
         interest: 'Food',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '6',
+        interest: 'Traveling',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '7',
+        interest: 'Musicals',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '8',
+        interest: 'Code',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '9',
+        interest: 'Second hand',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '10',
+        interest: 'Board games',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '11',
+        interest: 'Family',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '12',
+        interest: 'Mother Nature',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '13',
+        interest: 'Yoga',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '14',
+        interest: 'Scuba Diving',
+        fill: getRandomFill(),
+        size: getRandomSize(),
+        position: getRandomPosition(),
+        svgSpeed: getRandomSvgSpeed(),
+        moveSpeed: getRandomMoveSpeed(),
+        delay: getDelay(),
+    },
+    {
+        key: '15',
+        interest: 'Adventure',
         fill: getRandomFill(),
         size: getRandomSize(),
         position: getRandomPosition(),
