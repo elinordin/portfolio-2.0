2ce890f4260fc763448b7935a65cd5a698833c2f
elinordin <elin.evelina@live.se>
2021-04-15 09:57:35 +0200

    Add navigate back to center arrow in header for mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Switches from '../components/switches.jsx'
+import { ReactComponent as ChevronLeft } from '../assets/icons/chevron-left.svg'
 
 function Header(props) {
 
     return (
         <header className={`${props.position}-header`}>
-            <div className={props.position === 'right'? 'slide-out' : undefined}>
-                <h1 onClick={() => {props.navigate('center')}}>Elin Nordin</h1>
-                <h2>frontend developer</h2>
+            <div className='arrow-text-container'>
+                {props.position != 'center' &&
+                    <button
+                        type='button'
+                        aria-label='Go back to start page'
+                        className='nav-button'
+                        onClick={() => props.navigate('center')}
+                    >
+                        <ChevronLeft className='arrow' />
+                    </button>}
+
+                <div className={props.position === 'right' ? 'slide-out' : undefined}>
+                    <h1>Elin Nordin</h1>
+                    <h2>frontend developer</h2>
+                </div>
             </div>
-            <Switches theme={props.theme} music={props.music} toggleSwitch={props.toggleSwitch}/>
+
+            <Switches theme={props.theme} music={props.music} toggleSwitch={props.toggleSwitch} />
         </header>
     );
 }
