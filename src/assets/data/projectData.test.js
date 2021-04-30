7bb26eaa87bb5cd0c01ccca2ad2790a058607c41
elinordin <elin.evelina@live.se>
2021-04-30 17:58:43 +0200

    Add tests to projectData.js

---
+++ src/assets/data/projectData.test.js
@@ -0,0 +1,33 @@
+import projects from './projectData'
+
+describe('Projects', () => {
+
+    test('exists', () => {
+        expect(projects).toBeDefined()
+    })
+
+    test('contains 6 items', () => {
+        expect(projects.length).toBe(6)
+    })
+
+})
+
+projects.forEach((project, index) => {
+
+    describe(`Project ${index + 1}`, () => {
+
+        test('contains a key', () => {
+            expect(project).toHaveProperty('key')
+        })
+
+        test('contains an image', () => {
+            expect(project).toHaveProperty('image')
+        })
+
+        test('contains an alt-text', () => {
+            expect(project).toHaveProperty('alt')
+        })
+
+    })
+    
+})
